3228e026ca4b3b04e159ab112fd83bd4f33e5657
Cody Hookey <codyhookey@outlook.com>
2025-06-01 15:39:29 +0100

    Added the Destroy feature to tweets as well as modification to users:
    - added a new way to get token, session and session.user as i was writing it out multiple times
    
    - added the delete route for tweets
    
    added the destoy function to tweets controller
    
    modified the return json from user as it was missing the email in the return

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,8 @@
 class TweetsController < ApplicationController
   def create
-    token = cookies.signed[:twitter_session_token]
-    session = Session.find_by(token: token)
 
-    if session
-      user = session.user
-      @tweet = user.tweets.new(tweet_params)
+    if current_user
+      @tweet = current_user.tweets.new(tweet_params)
 
       if @tweet.save
         render 'tweets/create'
@@ -17,9 +14,25 @@ class TweetsController < ApplicationController
     end
   end
 
+  def destroy
+    @tweet = Tweet.find_by(id: params[:id])
+
+    if @tweet.user == current_user and @tweet&.destroy
+      render json: { success: true}
+    else
+      render json: { success: false}
+    end
+  end
+
   private
 
   def tweet_params
     params.require(:tweet).permit(:message)
+  end
+
+  def current_user
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+    session&.user
   end
 end
